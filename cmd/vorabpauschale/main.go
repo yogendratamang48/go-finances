7ce0484128969df40b66e33dfc194ed7e6c695c2
yogendratamang48 <yogen48@hotmail.com>
2024-07-31 21:00:02 +0200

    Add voabpauschale - simple

--- cmd/vorabpauschale/main.go
+++ cmd/vorabpauschale/main.go
@@ -7,15 +7,19 @@ import (
 	"os"
 )
 
+var baseInterestRate float32 = 0.0255 // 2.55% for the year 2023
+var taxRate float32 = 0.26375         // 25% capital gains tax + 5.5% solidarity surcharge (no church tax added)
+var advancelumpSumFactor float32 = 0.70
+
 type Etf struct {
 	Name       string  `json:"name"`
 	StartValue float32 `json:"startValue"`
 	EndValue   float32 `json:"endValue"`
 }
 
-// Vorabpauschale (Advance lump sum) is 70% of the base interest rate (2.55% for 2023) of the value at the start of the year
+// Vorabpauschale (Advance lump sum) is 70%(advanceLumpSumFactor) of the base interest rate (2.55% for 2023) of the value at the start of the year
 func (n Etf) calculateAdvanceLumpsum() float32 {
-	return n.StartValue * 0.70 * 0.0255
+	return n.StartValue * advancelumpSumFactor * baseInterestRate
 }
 
 func (n Etf) calculateUnrealizedGain() float32 {
@@ -31,7 +35,7 @@ func (n Etf) calculateTaxableAmount() float32 {
 
 func (n Etf) calculateTax() float32 {
 
-	return n.calculateTaxableAmount() * 26.375 / 100
+	return n.calculateTaxableAmount() * taxRate
 }
 
 // load etfs array from json file
